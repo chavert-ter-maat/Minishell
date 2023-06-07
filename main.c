e9e3835a3d08cc9e3cd7821675106a89cb8b24d5
Flen Huisman <fhuisman@f1r3s1.codam.nl>
2023-06-07 11:30:10 +0200

    test

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fhuisman <fhuisman@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/05 17:03:18 by fhuisman      #+#    #+#                 */
-/*   Updated: 2023/06/06 16:59:10 by fhuisman      ########   odam.nl         */
+/*   Updated: 2023/06/07 11:29:25 by fhuisman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,5 @@ int	main(int argc, char *argv[], char *envp[])
 {
 	if (argc > 1)
 		{}//exit
+		
 }
