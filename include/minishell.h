2a6189f91b8b6824d3b37f09e5dfc2abe6bb9311
Flen Huisman <fhuisman@f1r3s1.codam.nl>
2023-06-07 14:09:27 +0200

    added a header file

---
+++ include/minishell.h
@@ -0,0 +1,24 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/ioctl.h>
+# include <signal.h>
+# include <dirent.h>
+# include <string.h>
+# include <termios.h>
+# include <curses.h>
+# include <term.h>
+# include <assert.h>
+# include <errno.h>
+# include <stdbool.h>
+
+#endif
