de456f22cbe7aaa6aa90fea804e0e3922d365215
Chavert Ter Maat <cter-maa@f1r3s2.codam.nl>
2023-06-22 12:15:38 +0200

    iets

--- src/executor/handle_multiple_commands.c
+++ src/executor/handle_multiple_commands.c
@@ -5,104 +5,20 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-// // Function to redirect input
-// void redirectInput(int* fdin, int* tmpin, const char* infile) {
-//     if (infile != NULL) {
-//         *fdin = open(infile, O_RDONLY);
-//     } else {
-//         // Use default input
-//         *fdin = dup(*tmpin);
-//     }
-// }
-
-// // Function to redirect output
-// int redirectOutput(int i, int numsimplecommands, int* fdout, int* tmpout, const char* outfile) {
-//     if (i == numsimplecommands - 1) {
-//         // Last simple command
-//         if (outfile != NULL) {
-//             *fdout = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-//         } else {
-//             // Use default output
-//             *fdout = dup(*tmpout);
-//         }
-//     } else {
-//         // Not the last simple command
-//         // Create a pipe
-//         int fdpipe[2];
-//         pipe(fdpipe);
-//         *fdout = fdpipe[1];
-//         *fdin = fdpipe[0];
-//     }
-//     return *fdout;
-// }
-
-// // Function to execute a simple command
-// void executeSimpleCommand(const char* command, char* const arguments[]) {
-//     execvp(command, arguments);
-//     perror("execvp");
-//     _exit(1);
-// }
-
-// // Main execute function
-// void execute() {
-//     // Save stdin and stdout
-//     int tmpin = dup(0);
-//     int tmpout = dup(1);
-
-//     int fdin;
-//     int ret;
-//     int fdout;
-
-//     int i = 0;
-//     while (i < numsimplecommands) {
-//         // Redirect input
-//         redirectInput(&fdin, &tmpin, scmd[i].infile);
-//         close(fdin);
-
-//         // Setup output
-//         fdout = redirectOutput(i, numsimplecommands, &fdout, &tmpout, scmd[i].outfile);
-//         close(fdout);
-
-//         // Redirect output
-//         dup2(fdout, 1);
-//         close(fdout);
-
-//         // Create child process
-//         ret = fork();
-//         if (ret == 0) {
-//             executeSimpleCommand(scmd[i].args[0], scmd[i].args);
-//         }
-
-//         i++;
-//     }
-
-//     // Restore stdin and stdout defaults
-//     dup2(tmpin, 0);
-//     dup2(tmpout, 1);
-//     close(tmpin);
-//     close(tmpout);
-
-//     if (!background) {
-//         // Wait for the last command
-//         waitpid(ret, NULL, 0);
-//     }
-// }
-
-
-static int handle_fds(int *pipe_fd, int former_read_end)
+static int handle_fds(int *pipe_fd, int read_end)
 {
     if (close(pipe_fd[WRITE_END]) == FAILED)
         perror_exit("4: close");
 		
-    if (former_read_end != STDIN_FILENO) // Only close if it's not STDIN_FILENO
+    if (read_end != STDIN_FILENO) 
     {
-        if (close(former_read_end) == FAILED)
+        if (close(read_end) == FAILED)
             perror_exit("3: close");
     }
-	former_read_end = pipe_fd[READ_END];
-    return (former_read_end);
+	read_end = pipe_fd[READ_END];
+    return (read_end);
 }
-static int	execute_last_command(t_shell *shell, int former_read_end, char *cmd)
+static int	execute_last_command(t_shell *shell, int read_end, char *cmd)
 {
 	pid_t	pid;
 
@@ -111,18 +27,18 @@ static int	execute_last_command(t_shell *shell, int former_read_end, char *cmd)
 		perror_exit("fork");
 	if (pid == SUCCES)
 	{
-		if (dup2(former_read_end, STDIN_FILENO) == FAILED)
+		if (dup2(read_end, STDIN_FILENO) == FAILED)
 			perror_exit("3: dup2");
 		run_command(shell, cmd); 
 	}	
-	if (close(former_read_end) == FAILED)
+	if (close(read_end) == FAILED)
 		perror_exit("2: close");
 	return(pid);
 }
 
-static void execute_children(t_shell *shell, int former_read_end, int *pipe_fd, char *cmd)
+static void execute_child(t_shell *shell, int read_end, int *pipe_fd, char *cmd)
 {
-	if (dup2(former_read_end, STDIN_FILENO) == FAILED)
+	if (dup2(read_end, STDIN_FILENO) == FAILED)
 		perror_exit("2: dup2");
 	if (dup2(pipe_fd[WRITE_END], STDOUT_FILENO) == FAILED)
 		perror_exit("1: dup2");
@@ -135,10 +51,10 @@ static int	initiate_forks(t_shell *shell, int nb_commands, char **argv)
 {
 	int 	pipe_fd[2];
 	pid_t	pid;
-	int		former_read_end;
+	int		read_end;
 	int		index_command = 2;
 	
-	while(nb_commands != 0) // nb_commands  = 3
+	while(nb_commands) 
 	{
 	if (pipe(pipe_fd) == FAILED)
 		perror_exit("pipe");
@@ -146,12 +62,12 @@ static int	initiate_forks(t_shell *shell, int nb_commands, char **argv)
 	if (pid == FAILED)
 		perror_exit("fork");
 	if (pid == SUCCES)
-		execute_children(shell, former_read_end, pipe_fd, argv[index_command]);
-	former_read_end = handle_fds(pipe_fd, former_read_end);
+		execute_child(shell, read_end, pipe_fd, argv[index_command]);
+	read_end = handle_fds(pipe_fd, read_end);
 		nb_commands--;
 		index_command++;
 	}
-	pid = execute_last_command(shell, former_read_end, argv[index_command]);
+	pid = execute_last_command(shell, read_end, argv[index_command]);
 	return(pid);
 }
 
--- src/main.c
+++ src/main.c
@@ -1,23 +1,5 @@
 #include "../include/minishell.h"
 
-// int	main(int argc, char *argv[], char *envp[])
-// {
-// 	t_shell	shell;
-
-// 	(void) argv;
-// 	(void) envp;
-// 	if (argc > 1)
-// 		;//exit
-// 	while (1)
-// 	{
-// 		shell.cmd_line = readline("shellyeah$ ");
-// 		add_history(shell.cmd_line);
-// 		shell.tokens = lexer(&shell);
-// 		free(shell.cmd_line);
-// 	}
-// }
-
-// executor tester
 int main(int argc, char **argv, char **envp)
 {
 	t_shell shell;
