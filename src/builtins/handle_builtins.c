15e3ac6014053be72da739aa7adbf7c60d1086e0
Chavert Ter Maat <cter-maa@f1r3s2.codam.nl>
2023-07-18 13:35:13 +0200

    builtins env, echo, pwd working

--- src/builtins/handle_builtins.c
+++ src/builtins/handle_builtins.c
@@ -1,36 +1,38 @@
 #include "../../include/minishell.h"
 
-// static t_builtins *find_function(char *cmd)
-// {
-// 	int	index;
-// 	index = 0;
-
-// 	static const t_builtins	jump_table[] = 
-// 	{
-// 	// {.type_builtin = "cd", .function = ft_cd},
-// 	{.type_builtin = "echo", .function = ft_echo},
-// 	// {.type_builtin = "export", .function = ft_export},
-// 	{.type_builtin = "pwd", .function = ft_pwd},
-// 	{.type_builtin = "unset", .function = ft_unset},
-// 	{.type_builtin = "env", .function = ft_env},
-// 	// {.type_builtin = "exit", .function = ft_exit},
-// 	{NULL, NULL}
-// 	};
-// 	while(jump_table[index] && strncmp(jump_table[index].type_builtin), cmd,
-// 		ft_strlen(cmd) + 1);
-// 		index++;
-// 	return(jump_table[index]);
-// }
-
-// int	execute_builtins(t_shell *shell, char **arguments)
-// {
-// 	t_builtins *builtin_function;
-
-// 	if(arguments == NULL || *arguments == NULL)
-// 		return(ERROR); 
-// 	builtin_function = find_function(arguments[0]);
-// 	if(builtin_function == NULL)
-// 		return(ERROR);
-// 	builtin_function->function(shell, arguments);
-// 	return(SUCCESS);
-// }
+int execute_builtin(t_shell *shell)
+{
+    // if (ft_strncmp(shell->command->args[0], "cd", 3) == 0)
+    //  return (ft_cd));
+    if (ft_strncmp(shell->command->args[0], "echo", 5) == 0)
+        return (ft_echo(shell->command->args, 1));
+    if (ft_strncmp(shell->command->args[0], "env", 4) == 0)
+        return (ft_env(shell));
+    if (ft_strncmp(shell->command->args[0], "export", 7) == 0)
+     return (ft_export(shell->command, &shell->env_list));
+    if (ft_strncmp(shell->command->args[0], "pwd", 4) == 0)
+        return (ft_pwd());
+    if (ft_strncmp(shell->command->args[0], "unset", 6) == 0)
+     return (ft_unset(shell->command, &shell->env_list));
+    // if (ft_strncmp(shell->command->args[0], "exit", 5) == 0)
+    //  return (TRUE);
+    return (SUCCESS);
+}
+int check_if_builtin(char *command)
+{
+    if (ft_strncmp(command, "cd", 3) == 0)
+        return (TRUE);
+    if (ft_strncmp(command, "echo", 5) == 0)
+        return (TRUE);
+    if (ft_strncmp(command, "env", 4) == 0)
+        return (TRUE);
+    if (ft_strncmp(command, "export", 7) == 0)
+        return (TRUE);
+    if (ft_strncmp(command, "pwd", 4) == 0)
+        return (TRUE);
+    if (ft_strncmp(command, "unset", 6) == 0)
+        return (TRUE);
+    if (ft_strncmp(command, "exit", 5) == 0)
+        return (TRUE);
+    return (FALSE);
+}
--- src/executor/executor.c
+++ src/executor/executor.c
@@ -1,11 +1,18 @@
 #include "../../include/minishell.h"
 
-void    executor(t_shell *shell)
+int	executor(t_shell *shell)
 {
     if (!shell->command)
-        return;
-    if (!shell->command->next)
+        return(ERROR);
+    if (check_if_builtin(shell->command->args[0]) == TRUE)
+    {
+		if(execute_builtin(shell) == 1)
+			return(ERROR);
+		ft_putstr_fd("ft builtin executed\n", 1);
+    }
+    else if (!shell->command->next)
         handle_single_command(shell);
     else
         handle_multiple_commands(shell);
+	return (SUCCESS);
 }
