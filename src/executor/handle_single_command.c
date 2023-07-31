f007318a2b8d3d5278661bb56cb948827958b57d
Chavert home <chavert@live.nl>
2023-07-31 13:55:12 +0200

    exectutor single command works again

--- src/executor/handle_single_command.c
+++ src/executor/handle_single_command.c
@@ -6,22 +6,23 @@ void	handle_single_command(t_shell *shell, t_command *command)
 	int		status;
 
 	// save_std(shell->executor);
-
 	if (check_if_builtin(command->args[0]))
-        execute_builtin(shell, command);
+	{
+		execute_builtin(shell, command);
+		return ;
+	}
     else 
 	{
+
 		pid = fork();
 		if (pid == FAILED)
 			perror_exit(shell, "fork");
 		if (pid == SUCCESS)
-    	{
-
 		    execute_non_builtin(shell, shell->command_list->head->data);
-			if (waitpid(pid, &status, 0) == FAILED)
+		if (waitpid(pid, &status, 0) == FAILED)
 				perror_exit(shell, "waitpid");
-		}
-    }
+	}
+}
 	// restore_std(shell->executor);
 	// print_status_waidpid(pid, status);
-}
+// }
