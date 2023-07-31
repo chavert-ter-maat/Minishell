2f8493c7a84e0fd48975df07bb165a616fc298ba
Chavert home <chavert@live.nl>
2023-07-31 12:43:16 +0200

    fixed single command & execute non builtin

--- src/executor/handle_single_command.c
+++ src/executor/handle_single_command.c
@@ -5,27 +5,23 @@ void	handle_single_command(t_shell *shell, t_command *command)
 	pid_t	pid;
 	int		status;
 
-	pid = fork();
-	if (pid == FAILED)
-		perror_exit(shell, "fork");
 	// save_std(shell->executor);
-	if (pid == SUCCESS)
-    {
-        if (check_if_builtin(command->args[0]))
-            execute_builtin(shell, command);
-        else 
-		{
+
+	if (check_if_builtin(command->args[0]))
+        execute_builtin(shell, command);
+    else 
+	{
+		pid = fork();
+		if (pid == FAILED)
+			perror_exit(shell, "fork");
+		if (pid == SUCCESS)
+    	{
+
 		    execute_non_builtin(shell, shell->command_list->head->data);
 			if (waitpid(pid, &status, 0) == FAILED)
-				perror_exit("waitpid");
+				perror_exit(shell, "waitpid");
 		}
     }
 	// restore_std(shell->executor);
-<<<<<<< HEAD
-
-=======
-	if (waitpid(pid, &status, 0) == FAILED)
-		perror_exit(shell, "waitpid");
->>>>>>> 729c1cb79aa29c5d28d10e10fbffccdf779b3faf
 	// print_status_waidpid(pid, status);
 }
