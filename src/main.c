d9d40ba451f975d4a5229b8926b7107adf3097ac
Chavert Ter Maat <cter-maa@f1r3s2.codam.nl>
2023-08-01 12:55:42 +0200

    signals work

--- src/main.c
+++ src/main.c
@@ -37,11 +37,11 @@ int main(int argc, char **argv, char **envp)
 	init_env(&shell, envp);
 	while (1)
 	{
-		// init_signals();
+		init_signals();
 		shell.cmd_line = readline("shellyeah$ ");
-		// if (shell.cmd_line == NULL)
-		// 	sigquit_handler(&shell);
-		// else
+		if (shell.cmd_line == NULL)
+			sigquit_handler(&shell);
+		else
 		// {
 			add_history(shell.cmd_line);
 			parser(&shell);
