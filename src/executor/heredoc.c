b88d39d932392cc0fef2a7675b6866a7407e2404
Chavert Ter Maat <cter-maa@f1r5s5.codam.nl>
2023-08-24 15:12:16 +0200

    heredoc fix, builtin | not working

--- src/executor/heredoc.c
+++ src/executor/heredoc.c
@@ -1,32 +1,32 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        ::::::::            */
-/*   heredoc.c                                          :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: cter-maa <cter-maa@student.codam.nl>         +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2023/08/23 16:49:47 by cter-maa      #+#    #+#                 */
-/*   Updated: 2023/08/24 11:35:51 by fhuisman      ########   odam.nl         */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "../../include/minishell.h"
 
 static void	here_wait(t_shell *shell, pid_t pid, int status)
 {
 	if (waitpid(pid, &status, 0) == FAILED)
-		return (perror_update_status(shell, "waitpid"));
+		perror_exit_fork(shell, "waitpid");
 	if (WIFSIGNALED(status))
 		shell->status = g_status;
 	else
 		shell->status = WEXITSTATUS(status);
 }
 
+static void	sigint_handler_heredoc(int signum)
+{
+	(void) signum;
+	_exit(1);
+}
+
+static void	init_signals_heredoc(void)
+{
+	signal(SIGINT, &sigint_handler_heredoc);
+	signal(SIGTSTP, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
+}
+
 static void	write_to_pipe(t_shell *shell, char *delimiter, int pipe_fd)
 {
 	char	*line;
 
-	init_signals(HEREDOC);
 	line = readline("> ");
 	while (line && !(ft_strncmp(line, delimiter,
 				ft_strlen(delimiter) + 1) == 0))
@@ -50,6 +50,8 @@ void	create_heredoc(t_shell *shell, t_redir *redir)
 	pid_t	pid;
 
 	status = 0;
+	if (redir->heredoc_read_end)
+		close(redir->heredoc_read_end);
 	if (!redir->file || redir->file[0] == '\0')
 		return (perror_update_status(shell, "close"));
 	if (pipe(pipe_fd) == FAILED)
@@ -59,11 +61,10 @@ void	create_heredoc(t_shell *shell, t_redir *redir)
 		return (perror_update_status(shell, "fork"));
 	if (pid == SUCCESS)
 	{
-		if (close(pipe_fd[READ_END] == FAILED))
-			return (perror_update_status(shell, "close"));
+		init_signals_heredoc();
 		write_to_pipe(shell, redir->file, pipe_fd[WRITE_END]);
 	}
-	else if (close(pipe_fd[WRITE_END]) == FAILED)
+	if (close(pipe_fd[WRITE_END]) == FAILED)
 		perror_exit_fork(shell, "close");
 	here_wait(shell, pid, status);
 	redir->heredoc_read_end = pipe_fd[READ_END];
